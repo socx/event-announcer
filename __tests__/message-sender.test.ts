4dceacaa2a5ee8e00296615e007d45c4faae1d4a
Socx Musterion <socx9@yahoo.com>
2025-08-04 10:36:35 +0100

    tests for getTodayCelebrants

--- __tests__/message-sender.test.ts
+++ __tests__/message-sender.test.ts
@@ -1,4 +1,8 @@
-import { FamilyMember, getMonthCelebrants } from '../src/lib/event-reminder/message-sender';
+import {
+  FamilyMember,
+  getMonthCelebrants,
+  getTodayCelebrants,
+} from '../src/lib/event-reminder/message-sender';
 
 
 describe('getMonthCelebrants', () => {
@@ -72,6 +76,88 @@ describe('getMonthCelebrants', () => {
       { id: '2', firstname: 'Jane', middlename: '', lastname: 'Smith', gender: 'Female', parents: [], birthDate: new Date(today.getFullYear(), currentMonth + 1, 10), weddingDate: new Date(today.getFullYear(), currentMonth - 1, 25), deathDate: undefined, spouses: [] },
     ];
     const { birthdays, anniversaries } = getMonthCelebrants(familyMembers);
+
+    expect(birthdays).toEqual([]);
+    expect(anniversaries).toEqual([]);
+  });
+});
+
+
+describe('getTodayCelebrants', () => {
+  it('should return empty arrays when no family members are provided', () => {
+    const familyMembers: FamilyMember[] = [];
+    const { birthdays, anniversaries } = getTodayCelebrants(familyMembers);
+
+    expect(birthdays).toEqual([]);
+    expect(anniversaries).toEqual([]);
+  });
+
+  it('should return empty arrays when family members have no valid dates', () => {
+    const familyMembers: FamilyMember[] = [
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: undefined, weddingDate: undefined, deathDate: undefined, spouses: [] },
+      { id: '2', firstname: 'Jane', middlename: '', lastname: 'Smith', gender: 'Female', parents: [], birthDate: undefined, weddingDate: undefined, deathDate: undefined, spouses: [] },
+    ];
+    const { birthdays, anniversaries } = getTodayCelebrants(familyMembers);
+
+    expect(birthdays).toEqual([]);
+    expect(anniversaries).toEqual([]);
+  });
+
+  it('should return birthdays for today', () => {
+    const today = new Date();
+
+    const familyMembers: FamilyMember[] = [
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: today, weddingDate: undefined, deathDate: undefined, spouses: [] },
+      { id: '2', firstname: 'Jane', middlename: '', lastname: 'Smith', gender: 'Female', parents: [], birthDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), weddingDate: undefined, deathDate: undefined, spouses: [] },
+    ];
+    const { birthdays, anniversaries } = getTodayCelebrants(familyMembers);
+
+    expect(birthdays).toEqual([
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: today, weddingDate: undefined, deathDate: undefined, spouses: [] },
+    ]);
+    expect(anniversaries).toEqual([]);
+  });
+
+  it('should return anniversaries for today', () => {
+    const today = new Date();
+
+    const familyMembers: FamilyMember[] = [
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: undefined, weddingDate: today, deathDate: undefined, spouses: [] },
+      { id: '2', firstname: 'Jane', middlename: '', lastname: 'Smith', gender: 'Female', parents: [], birthDate: undefined, weddingDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), deathDate: undefined, spouses: [] },
+    ];
+    const { birthdays, anniversaries } = getTodayCelebrants(familyMembers);
+
+    expect(birthdays).toEqual([]);
+    expect(anniversaries).toEqual([
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: undefined, weddingDate: today, deathDate: undefined, spouses: [] },
+    ]);
+  });
+
+  it('should return both birthdays and anniversaries for today', () => {
+    const today = new Date();
+
+    const familyMembers: FamilyMember[] = [
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: today, weddingDate: today, deathDate: undefined, spouses: [] },
+      { id: '2', firstname: 'Jane', middlename: '', lastname: 'Smith', gender: 'Female', parents: [], birthDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), weddingDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), deathDate: undefined, spouses: [] },
+    ];
+    const { birthdays, anniversaries } = getTodayCelebrants(familyMembers);
+
+    expect(birthdays).toEqual([
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: today, weddingDate: today, deathDate: undefined, spouses: [] },
+    ]);
+    expect(anniversaries).toEqual([
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: today, weddingDate: today, deathDate: undefined, spouses: [] },
+    ]);
+  });
+
+  it('should exclude birthdays and anniversaries not matching today', () => {
+    const today = new Date();
+
+    const familyMembers: FamilyMember[] = [
+      { id: '1', firstname: 'John', middlename: '', lastname: 'Doe', gender: 'Male', parents: [], birthDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), weddingDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1), deathDate: undefined, spouses: [] },
+      { id: '2', firstname: 'Jane', middlename: '', lastname: 'Smith', gender: 'Female', parents: [], birthDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1), weddingDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1), deathDate: undefined, spouses: [] },
+    ];
+    const { birthdays, anniversaries } = getTodayCelebrants(familyMembers);
 
     expect(birthdays).toEqual([]);
     expect(anniversaries).toEqual([]);
